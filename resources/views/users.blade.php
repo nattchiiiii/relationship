a15c5555fa27a50439cf5ece0e4b61b2ba0b4f70
Febi Salsabila <febisalsabillah05@gmail.com>
2025-01-26 11:18:03 +0700

    one to many

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -11,8 +11,7 @@
     <div class="container">
         <div class="card mt-5">
             <div class="card-body">
-                <h3 class="text-center"><a href="https://santrikoding.com">www.santrikoding.com</a></h3>
-                <h5 class="text-center my-4">Laravel Eloquent Relationship : One To One</h5>
+                  <h5 class="text-center my-4">Laravel Eloquent Relationship: One To One</h5>
                 <table class="table table-bordered table-striped">
                     <thead>
                         <tr>
